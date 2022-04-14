95d923834be6d27daa7dc073df2321cb5c0072c2
Maxlego08 <maxlego08@sfr.fr>
2022-04-14 19:22:04 +0200

    :construction: Add ZButtonManager

--- src/fr/maxlego08/menu/api/loader/ButtonLoader.java
+++ src/fr/maxlego08/menu/api/loader/ButtonLoader.java
@@ -1,5 +1,23 @@
 package fr.maxlego08.menu.api.loader;
 
+import org.bukkit.plugin.Plugin;
+
+import fr.maxlego08.menu.api.button.Button;
+
 public interface ButtonLoader {
 
+	/**
+	 * Returns the class that will be used for the button
+	 * 
+	 * @return classz
+	 */
+	public Class<? extends Button> getButton();
+
+	/**
+	 * Return the plugin where the loader button comes from
+	 * 
+	 * @return plugin
+	 */
+	public Plugin getPlugin();
+	
 }
