0f1b743516ed5d9a5234857f6a10b85b59fe2a3a
Maxlego08 <maxlego08@sfr.fr>
2022-04-14 14:33:32 +0200

    :construction: Add ZButton

---
+++ src/fr/maxlego08/menu/buttons/ZButton.java
@@ -0,0 +1,69 @@
+package fr.maxlego08.menu.buttons;
+
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import fr.maxlego08.menu.api.buttons.Button;
+import fr.maxlego08.menu.zcore.utils.ZUtils;
+
+public abstract class ZButton extends ZUtils implements Button {
+
+	private final String buttonName;
+	private final ItemStack itemStack;
+	private final int slot;
+	private final boolean isPermanent;
+
+	/**
+	 * @param buttonName
+	 * @param itemStack
+	 * @param slot
+	 * @param isPermanent
+	 */
+	public ZButton(String buttonName, ItemStack itemStack, int slot, boolean isPermanent) {
+		super();
+		this.buttonName = buttonName;
+		this.itemStack = itemStack;
+		this.slot = slot;
+		this.isPermanent = isPermanent;
+	}
+
+	@Override
+	public String getName() {
+		return this.buttonName;
+	}
+
+	@Override
+	public ItemStack getItemStack() {
+		return this.itemStack;
+	}
+
+	@Override
+	public ItemStack getCustomItemStack(Player player) {
+		if (this.itemStack == null) {
+			return null;
+		}
+		ItemStack itemStack = this.itemStack.clone();
+		return super.playerHead(super.papi(itemStack, player), player);
+	}
+
+	@Override
+	public int getSlot() {
+		return this.slot;
+	}
+
+	@Override
+	public boolean isClickable() {
+		return true;
+	}
+
+	@Override
+	public boolean isPermament() {
+		return this.isPermanent;
+	}
+
+	@Override
+	public <T extends Button> T toButton(Class<T> classz) {
+		return (T) this;
+	}
+
+}
