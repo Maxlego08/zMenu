1d299b7a99b445883f870d3ca463b201fd2e9066
Maxlego08 <maxlego08@sfr.fr>
2022-04-14 13:44:45 +0200

    :construction: Start buttons

---
+++ src/fr/maxlego08/menu/api/buttons/Button.java
@@ -0,0 +1,46 @@
+package fr.maxlego08.menu.api.buttons;
+
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+public interface Button {
+
+	/**
+	 * Returns the name of the button
+	 * 
+	 * @return name
+	 */
+	public String getName();
+	
+	/**
+	 * Returns the itemstack that will be displayed
+	 * 
+	 * @return {@link ItemStack}
+	 */
+	public ItemStack getItemStack();
+	
+	/**
+	 * This method will return the itemstack that will be used in the inventory with a player in parameter
+	 * 
+	 * @param player Player who opens the inventory
+	 * @return {@link ItemStack}
+	 */
+	public ItemStack getCustomItemStack(Player player);
+	
+	/**
+	 * Allows to know if the button can be clicked
+	 * 
+	 * @return boolean
+	 */
+	public boolean isClickable();
+	
+	/**
+	 * Allows to know if the button is permanent
+	 * A permanent button will always be present in the inventory no matter the page
+	 * 
+	 * @return boolean
+	 */
+	public boolean isPermament();
+	
+	
+}
