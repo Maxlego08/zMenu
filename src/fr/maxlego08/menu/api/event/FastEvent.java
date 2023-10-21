c06f5fc8f537de2d4b6041a77d6c96663c31aef8
Maxlego08 <maxlego08@sfr.fr>
2023-10-21 13:20:33 +0200

    :memo: Change class type

--- src/fr/maxlego08/menu/api/event/FastEvent.java
+++ src/fr/maxlego08/menu/api/event/FastEvent.java
@@ -7,26 +7,32 @@ import fr.maxlego08.menu.api.event.events.PlayerOpenInventoryEvent;
 /**
  * Allows you to listen to the events of the plugin without using the Bukkit API, you gain performance
  */
-public interface FastEvent {
+public abstract class FastEvent {
 
     /**
      * Event when a button will be loaded
      *
      * @param event {@link ButtonLoadEvent}
      */
-    void onButtonLoad(ButtonLoadEvent event);
+    public void onButtonLoad(ButtonLoadEvent event){
+
+    }
 
     /**
      * Event when an inventory will be loaded
      *
      * @param event {@link InventoryLoadEvent}
      */
-    void onInventoryLoad(InventoryLoadEvent event);
+    public void onInventoryLoad(InventoryLoadEvent event){
+
+    }
 
     /**
      * Event when a player open an inventory
      *
      * @param event {@link PlayerOpenInventoryEvent}
      */
-    void onPlayerOpenInventory(PlayerOpenInventoryEvent event);
+    public void onPlayerOpenInventory(PlayerOpenInventoryEvent event){
+
+    }
 }
