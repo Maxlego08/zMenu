688588151ab1c4b6d7b330ad439b424a09991c80
Maxlego08 <maxlego08@sfr.fr>
2024-03-03 18:52:11 +0100

    :memo: Fix pagination

--- src/fr/maxlego08/menu/ZInventory.java
+++ src/fr/maxlego08/menu/ZInventory.java
@@ -109,12 +109,13 @@ public class ZInventory extends ZUtils implements Inventory {
         if (optional.isPresent()) {
             int maxSlot = optional.get();
             maxPage = (maxSlot / this.size) + 1;
+            maxPage = (int) Math.ceil((double) maxPage / this.size);
         }
 
         Optional<PaginateButton> optionalPaginate = this.buttons.stream().filter(button -> button instanceof PaginateButton).map(e -> (PaginateButton) e).sorted(Comparator.comparingInt(e -> ((PaginateButton) e).getPaginationSize(player)).reversed()).findFirst();
         if (optionalPaginate.isPresent()) {
             PaginateButton paginateButton = optionalPaginate.get();
-            maxPage = (paginateButton.getPaginationSize(player) / paginateButton.getSlots().size()) + 1;
+            maxPage = (int) Math.ceil((double) paginateButton.getPaginationSize(player) / paginateButton.getSlots().size());
         }
 
         return maxPage;
