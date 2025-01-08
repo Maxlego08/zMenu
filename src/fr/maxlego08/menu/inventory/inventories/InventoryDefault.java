2436efbaabf840463ce913cbb38ee9bb7e2a7adb
Traqueur-dev <traqueur.dev@gmail.com>
2025-01-08 18:11:39 +0100

    feat: add sysout

--- src/fr/maxlego08/menu/inventory/inventories/InventoryDefault.java
+++ src/fr/maxlego08/menu/inventory/inventories/InventoryDefault.java
@@ -89,11 +89,7 @@ public class InventoryDefault extends VInventory {
             }
 
             // Display buttons
-            this.buttons.forEach(button -> {
-                System.out.println("forEach");
-                System.out.println(button.getSlots());
-                this.buildButton(button);
-            });
+            this.buttons.forEach(this::buildButton);
 
             if (isAsync) {
                 scheduler.runTask(player.getLocation(), () -> {
@@ -141,6 +137,7 @@ public class InventoryDefault extends VInventory {
      * @param button The button
      */
     public void buildButton(Button button) {
+
         if (button.hasCustomRender()) {
             button.onRender(player, this);
             return;
@@ -202,6 +199,7 @@ public class InventoryDefault extends VInventory {
 
         ItemStack itemStack = button.getCustomItemStack(this.player);
         for (int slot : slots) {
+
             if (slot < 0) {
                 Logger.info("slot is negative ! (" + slot + ") Button: " + button.getName() + " in inventory " + this.inventory.getFileName(), Logger.LogType.ERROR);
                 continue;
