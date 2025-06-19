48537ae92c6ee2bb58bbc7a3c751977942e6c451
Maxlego08 <maxlego08@sfr.fr>
2025-06-19 11:37:43 +0200

    :memo: javadocs

--- API/src/main/java/fr/maxlego08/menu/api/InventoryOption.java
+++ API/src/main/java/fr/maxlego08/menu/api/InventoryOption.java
@@ -6,8 +6,24 @@ import java.io.File;
 
 public interface InventoryOption {
 
+    /**
+     * Returns the name of this option.
+     * This name is used to identify the option in the configuration file.
+     * The name is case-sensitive.
+     *
+     * @return The name of this option.
+     */
     String getName();
 
+    /**
+     * Loads the given inventory from the given configuration file
+     *
+     * @param inventory           the inventory to load
+     * @param file                the file that the configuration is loaded from
+     * @param configuration       the configuration to load the inventory from
+     * @param inventoryManager    the inventory manager to use for inventory operations
+     * @param buttonManager       the button manager to use for button-related operations
+     */
     void loadInventory(Inventory inventory, File file, YamlConfiguration configuration, InventoryManager inventoryManager, ButtonManager buttonManager);
 
 }
--- API/src/main/java/fr/maxlego08/menu/api/MenuItemStack.java
+++ API/src/main/java/fr/maxlego08/menu/api/MenuItemStack.java
@@ -2,11 +2,7 @@ package fr.maxlego08.menu.api;
 
 import fr.maxlego08.menu.api.attribute.IAttribute;
 import fr.maxlego08.menu.api.enums.MenuItemRarity;
-import fr.maxlego08.menu.api.itemstack.Banner;
-import fr.maxlego08.menu.api.itemstack.Firework;
-import fr.maxlego08.menu.api.itemstack.LeatherArmor;
-import fr.maxlego08.menu.api.itemstack.Potion;
-import fr.maxlego08.menu.api.itemstack.TrimConfiguration;
+import fr.maxlego08.menu.api.itemstack.*;
 import fr.maxlego08.menu.api.utils.LoreType;
 import fr.maxlego08.menu.api.utils.Placeholders;
 import org.bukkit.OfflinePlayer;
@@ -20,24 +16,85 @@ import java.util.Map;
 
 public interface MenuItemStack {
 
+    /**
+     * Build an item stack using the provided player context.
+     * This method constructs an ItemStack instance for the player,
+     * applying any necessary configurations and customizations.
+     *
+     * @param player The player for whom the item stack is being built.
+     * @return The constructed ItemStack for the specified player.
+     */
     ItemStack build(Player player);
 
+    /**
+     * Build an item stack using the given player and whether to use the cache or not.
+     *
+     * @param player   The player to use to build the item stack.
+     * @param useCache Whether to use the cache or not.
+     * @return The built item stack.
+     */
     ItemStack build(Player player, boolean useCache);
 
+    /**
+     * Build an item stack using the given player, use cache and placeholders.
+     *
+     * @param player       The player to use to build the item stack.
+     * @param useCache     Whether to use the cache or not.
+     * @param placeholders The placeholders to use in the item stack construction.
+     * @return The built item stack.
+     */
     ItemStack build(Player player, boolean useCache, Placeholders placeholders);
 
+    /**
+     * Retrieves the {@link InventoryManager} responsible for managing this menu item stack.
+     *
+     * @return The inventory manager that manages this menu item stack.
+     */
     InventoryManager getInventoryManager();
 
+    /**
+     * Returns the path of the configuration file associated with the menu item stack.
+     *
+     * @return The path of the configuration file.
+     */
     String getFilePath();
 
+    /**
+     * Returns the path of the configuration file associated with the menu item stack,
+     * relative to the root of the configuration folder.
+     *
+     * @return The path of the configuration file relative to the root of the configuration folder.
+     */
     String getPath();
 
+    /**
+     * Returns the material of the item stack.
+     *
+     * @return The material of the item stack.
+     */
     String getMaterial();
 
+    /**
+     * Sets the material of the item stack.
+     *
+     * @param material The new material.
+     */
     void setMaterial(String material);
 
+    /**
+     * Retrieves the target player placeholder used in the menu item stack,
+     * or {@code null} if no target player placeholder is set.
+     *
+     * @return The target player placeholder, or {@code null} if none is set.
+     */
     String getTargetPlayer();
 
+    /**
+     * Sets the target player placeholder for the menu item stack.
+     * This placeholder is used to represent the target player in the menu configurations.
+     *
+     * @param targetPlayer The target player placeholder to set.
+     */
     void setTargetPlayer(String targetPlayer);
 
     String getAmount();
