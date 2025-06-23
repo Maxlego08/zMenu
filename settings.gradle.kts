25a4b8d294786b8cb7a71154f8a71e15b7193e06
Alwyn974 <alwyn974@gmail.com>
2025-06-23 21:27:57 +0400

    build(gradle): integrate custom plugin and refine repository configuration
    - Added `re.alwyn974.groupez.repository` plugin with version `1.0.0-SNAPSHOT`.
    - Removed direct Maven repository declarations for Groupez and migrated them to `pluginManagement` in `settings.gradle.kts`.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,18 @@ rootProject.name = "zMenu"
 
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
+pluginManagement {
+    repositories {
+        maven {
+            name = "groupez-releases"
+            url = uri("https://repo.groupez.dev/releases")
+        }
+        gradlePluginPortal()
+        mavenLocal()
+    }
+}
+
+
 include("API")
 
 file("Hooks").listFiles()?.forEach { file ->
