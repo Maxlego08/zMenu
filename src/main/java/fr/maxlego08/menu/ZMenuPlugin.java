ccd2fc2723ea6fe47aa82e26faa99359aeb8757c
1robie <97293924+1robie@users.noreply.github.com>
2025-08-31 22:00:40 +0200

    feat: load dialog files only on Paper servers with version > 1.21.6

--- src/main/java/fr/maxlego08/menu/ZMenuPlugin.java
+++ src/main/java/fr/maxlego08/menu/ZMenuPlugin.java
@@ -302,10 +302,12 @@ public class ZMenuPlugin extends ZPlugin implements MenuPlugin {
         files.add("patterns/pattern_cookies.yml");
         files.add("patterns/playtime_reward.yml");
 
-        files.add("dialogs/confirmation-dialog.yml");
-        files.add("dialogs/default-dialog.yml");
-        files.add("dialogs/multi_action-dialog.yml");
-        files.add("dialogs/server_link-dialog.yml");
+        if (isPaper() && NmsVersion.getCurrentVersion().isDialogsVersion()){
+            files.add("dialogs/confirmation-dialog.yml");
+            files.add("dialogs/default-dialog.yml");
+            files.add("dialogs/multi_action-dialog.yml");
+            files.add("dialogs/server_link-dialog.yml");
+        }
 
         return files;
     }
