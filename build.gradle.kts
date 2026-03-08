bfe2726179f1a0f1c90c1898f7c2293a4a3ec5a3
Maxence <simon.maxence@hotmail.com>
2026-03-08 19:39:59 +0100

    :memo: Fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,15 +33,15 @@ allprojects {
         maven(url = "https://repo.codemc.io/repository/maven-snapshots/")
         maven(url = "https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
         maven(url = "https://hub.spigotmc.org/nexus/content/groups/public/")
+        maven(url = "https://repo.extendedclip.com/content/repositories/placeholderapi/")
+        maven(url = "https://libraries.minecraft.net/")
+        maven(url = "https://repo.jsinco.dev/releases")
         maven {
             url = uri("https://jitpack.io")
             content {
                 excludeGroup("org.spigotmc")
             }
         }
-        maven(url = "https://repo.extendedclip.com/content/repositories/placeholderapi/")
-        maven(url = "https://libraries.minecraft.net/")
-        maven(url = "https://repo.jsinco.dev/releases")
     }
 
     java {
