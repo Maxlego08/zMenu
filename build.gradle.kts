e7f47e03c12355d2273d020d0aee3e0768e3f601
Maxence <simon.maxence@hotmail.com>
2026-04-06 14:51:37 +0200

    :memo: Fix API

--- build.gradle.kts
+++ build.gradle.kts
@@ -109,7 +109,7 @@ allprojects {
         compileOnly("me.clip:placeholderapi:2.11.6")
 
         implementation("fr.maxlego08.sarah:sarah:1.22")
-        implementation("fr.traqueur.currencies:currenciesapi:1.0.11")
+        implementation("fr.traqueur.currencies:currenciesapi:1.0.13")
         implementation("com.tcoded:FoliaLib:0.5.1")
 
         implementation("com.github.cryptomorin:XSeries:13.3.0")
