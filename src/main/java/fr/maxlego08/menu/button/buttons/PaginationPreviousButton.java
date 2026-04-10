8f5d6d2a15dd11851a075f3240c036357b3fca49
1robie <97293924+1robie@users.noreply.github.com>
2026-04-10 10:58:59 +0200

    feat: add support for conditional refresh in pagination buttons + refractor them loading logic

--- src/main/java/fr/maxlego08/menu/button/buttons/PaginationPreviousButton.java
+++ src/main/java/fr/maxlego08/menu/button/buttons/PaginationPreviousButton.java
@@ -1,6 +1,7 @@
 package fr.maxlego08.menu.button.buttons;
 
 import fr.maxlego08.menu.api.MenuPlugin;
+import fr.maxlego08.menu.api.button.GenericPaginateButton;
 import fr.maxlego08.menu.api.engine.InventoryEngine;
 import fr.maxlego08.menu.api.utils.Placeholders;
 import org.bukkit.entity.Player;
@@ -9,25 +10,16 @@ import org.jetbrains.annotations.NotNull;
 
 public class PaginationPreviousButton extends PaginationButton {
 
-    public PaginationPreviousButton(@NotNull MenuPlugin plugin, @NotNull String contextId) {
-        super(plugin, contextId);
-    }
-
-    protected void onPreviousPage(@NotNull Player player, @NotNull InventoryEngine inventory) {
-        refreshInventory(player);
-    }
-
-    protected void onCannotPreviousPage(@NotNull Player player, @NotNull InventoryEngine inventory) {
+    public PaginationPreviousButton(@NotNull MenuPlugin plugin, @NotNull String contextId, boolean onlyRefreshButton) {
+        super(plugin, contextId, onlyRefreshButton);
     }
 
     @Override
     public void onClick(@NotNull Player player, @NotNull InventoryClickEvent event, @NotNull InventoryEngine inventory, int slot, @NotNull Placeholders placeholders) {
-        int currentPage = this.manager.getPage(player.getUniqueId(), this.contextId);
-        if (currentPage > 0) {
-            this.manager.previousPage(player.getUniqueId(), this.contextId);
-            onPreviousPage(player, inventory);
-        } else {
-            onCannotPreviousPage(player, inventory);
+        super.onClick(player, event, inventory, slot, placeholders);
+        GenericPaginateButton paginateButton = findPaginateButton(inventory, player);
+        if (paginateButton != null && paginateButton.previousPage(player)) {
+            onPageChange(player, inventory, paginateButton);
         }
     }
 }
