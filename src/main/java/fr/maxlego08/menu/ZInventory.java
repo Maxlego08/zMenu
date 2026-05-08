a84d288dfd86c9bf66c9c675446d97f929951715
1robie <97293924+1robie@users.noreply.github.com>
2026-05-08 12:05:30 +0200

    fix: add nullable annotation import in ZInventory

--- src/main/java/fr/maxlego08/menu/ZInventory.java
+++ src/main/java/fr/maxlego08/menu/ZInventory.java
@@ -20,6 +20,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
 import java.util.*;
