83ab5d1f9c32f6564f96fc747f40aa064c2caf46
1robie <97293924+1robie@users.noreply.github.com>
2026-06-22 18:23:18 +0200

    feat: code cleanup

--- Hooks/Bedrock/src/main/java/fr/maxlego08/menu/hooks/bedrock/loader/builder/BedrockBuilderManager.java
+++ Hooks/Bedrock/src/main/java/fr/maxlego08/menu/hooks/bedrock/loader/builder/BedrockBuilderManager.java
@@ -1,14 +1,6 @@
 package fr.maxlego08.menu.hooks.bedrock.loader.builder;
 
 import fr.maxlego08.menu.api.MenuPlugin;
-import fr.maxlego08.menu.api.button.buttons.bedrock.inputs.BedrockInputButton;
-import fr.maxlego08.menu.api.context.BedrockRenderContext;
-import fr.maxlego08.menu.api.utils.Placeholders;
-import org.bukkit.entity.Player;
-import org.geysermc.cumulus.component.Component;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public abstract class BedrockBuilderManager {
     protected final MenuPlugin menuPlugin;
@@ -17,30 +9,4 @@ public abstract class BedrockBuilderManager {
         this.menuPlugin = menuPlugin;
     }
 
-    protected List<Component> getInputComponents(Player player, List<BedrockInputButton> inputButtons, Placeholders placeholders) {
-        return this.buildComponents(
-                inputButtons,
-                player
-        );
-    }
-
-    protected <B extends BedrockInputButton> List<Component> buildComponents(
-            List<B> buttons,
-            Player player
-    ) {
-        List<Component> result = new ArrayList<>();
-        //TODO: remodifier
-        BedrockRenderContext bedrockRenderContext = new BedrockRenderContext(result, player, null, this.menuPlugin.getMetaUpdater(), new Placeholders(), this.menuPlugin);
-        for (B button : buttons) {
-            if (button.hasSpecialRender()) {
-                button.onRender(bedrockRenderContext);
-            } else {
-                Component build = button.build(bedrockRenderContext);
-                if (build != null) {
-                    result.add(build);
-                }
-            }
-        }
-        return result;
-    }
 }
