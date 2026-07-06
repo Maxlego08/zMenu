d2543074a249a16729215517f295349de2153bb9
1robie <97293924+1robie@users.noreply.github.com>
2026-07-06 16:54:56 +0200

    feat: enhance ZMenuPluginLoader to include Maven Central repository for dependency resolution

--- Hooks/Paper/src/main/java/fr/maxlego08/menu/ZMenuPluginLoader.java
+++ Hooks/Paper/src/main/java/fr/maxlego08/menu/ZMenuPluginLoader.java
@@ -5,13 +5,25 @@ import io.papermc.paper.plugin.loader.PluginLoader;
 import io.papermc.paper.plugin.loader.library.impl.MavenLibraryResolver;
 import org.eclipse.aether.artifact.DefaultArtifact;
 import org.eclipse.aether.graph.Dependency;
+import org.eclipse.aether.repository.RemoteRepository;
 
 public class ZMenuPluginLoader implements PluginLoader {
+
     @Override
     public void classloader(PluginClasspathBuilder classpathBuilder) {
-        MavenLibraryResolver resolver = new MavenLibraryResolver();
-        resolver.addDependency(new Dependency(new DefaultArtifact("org.mariadb.jdbc:mariadb-java-client:3.5.6"), null));
-        resolver.addDependency(new Dependency(new DefaultArtifact("org.reflections:reflections:0.10.2"), null));
+        final MavenLibraryResolver resolver = new MavenLibraryResolver();
+        resolver.addRepository(new RemoteRepository.Builder(
+                "central", "default", MavenLibraryResolver.MAVEN_CENTRAL_DEFAULT_MIRROR
+        ).build());
+        resolver.addDependency(new Dependency(
+                new DefaultArtifact("org.mariadb.jdbc:mariadb-java-client:3.5.6"),
+                null
+        ));
+
+        resolver.addDependency(new Dependency(
+                new DefaultArtifact("org.reflections:reflections:0.10.2"),
+                null
+        ));
 
         classpathBuilder.addLibrary(resolver);
     }
