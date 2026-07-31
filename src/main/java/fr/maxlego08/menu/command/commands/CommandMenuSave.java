737d9f882ff021c5b0f43363ccf540b47c398727
1robie <97293924+1robie@users.noreply.github.com>
2026-07-31 21:43:06 +0200

    feat: enhance CommandMenuSave to use Enum for type argument and improve player context handling

--- src/main/java/fr/maxlego08/menu/command/commands/CommandMenuSave.java
+++ src/main/java/fr/maxlego08/menu/command/commands/CommandMenuSave.java
@@ -6,13 +6,16 @@ import fr.maxlego08.menu.api.InventoryManager;
 import fr.maxlego08.menu.api.utils.Message;
 import fr.maxlego08.menu.common.enums.Permission;
 import fr.maxlego08.menu.common.utils.MessageUtils;
+import fr.robie.paperdispatch.argument.EnumArgument;
 import fr.robie.paperdispatch.command.CommandDispatch;
 import fr.robie.paperdispatch.command.CommandResultType;
 import fr.robie.paperdispatch.command.SubCommand;
-import io.papermc.paper.command.brigadier.Commands;
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Locale;
 
 public class CommandMenuSave extends SubCommand<ZMenuPlugin> {
 
@@ -20,25 +23,35 @@ public class CommandMenuSave extends SubCommand<ZMenuPlugin> {
         super(plugin, "save");
         this.setPermission(Permission.ZMENU_SAVE.getPermission());
         this.addRequiredArgument("item-name", StringArgumentType.string());
-        this.addRequiredArgument(Commands.argument("type", StringArgumentType.string())
-                .suggests((context, builder) -> builder.suggest("yml").suggest("base64").buildFuture()));
+        this.addOptionalArgument("type", new EnumArgument<>(SaveType.class));
         this.setPlayerOnly();
     }
 
     @Override
     protected @NotNull CommandResultType perform(@NotNull CommandDispatch<ZMenuPlugin> commandDispatch) {
+        Player senderAsPlayer = commandDispatch.getSenderAsPlayer();
+
+        if (senderAsPlayer == null) {
+            MessageUtils.message(commandDispatch.getPlugin(), commandDispatch.getSender(), "You must be a player to use this command.");
+            return CommandResultType.SUCCESS;
+        }
 
         InventoryManager inventoryManager = commandDispatch.getPlugin().getInventoryManager();
         String name = commandDispatch.getArgument("item-name", String.class);
-        String type = commandDispatch.getArgument("type", String.class);
+        SaveType type = commandDispatch.getArgument("type", SaveType.class);
 
-        ItemStack itemStack = commandDispatch.getPlayer().getItemInHand();
+        ItemStack itemStack = commandDispatch.getSenderAsPlayer().getItemInHand();
         if (itemStack.getType() == Material.AIR) {
             MessageUtils.message(commandDispatch.getPlugin(), commandDispatch.getSender(), Message.SAVE_ERROR_EMPTY);
             return CommandResultType.SUCCESS;
         }
 
-        inventoryManager.saveItem(commandDispatch.getSender(), itemStack, name, type);
+        inventoryManager.saveItem(commandDispatch.getSender(), itemStack, name, type.name().toLowerCase(Locale.ROOT));
         return CommandResultType.SUCCESS;
+    }
+
+    private enum SaveType {
+        YML,
+        BASE64
     }
 }
